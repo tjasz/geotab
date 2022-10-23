de727d7352b43c9cc4cdcc3cce10d205a364ad13
tjasz <nukorausa@hotmail.com>
2022-10-23 10:20:08 -0700

    Add layers control to map

--- src/mapview.js
+++ src/mapview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, LayersControl } from 'react-leaflet';
 
 class MapView extends React.Component {
     constructor(props) {
@@ -15,11 +15,35 @@ class MapView extends React.Component {
     render() {
       return (
         <div id="mapview">
-          <MapContainer center={[45.4, -75.7]} zoom={12}scrollWheelZoom={false}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
+          <MapContainer center={[47.5, -122.3]} zoom={10} scrollWheelZoom={true}>
+            <LayersControl position="topright">
+              <LayersControl.BaseLayer checked name="OpenStreetMap">
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+              </LayersControl.BaseLayer>
+              <LayersControl.BaseLayer name="OpenTopoMap">
+                <TileLayer
+                    url="https://a.tile.opentopomap.org/{z}/{x}/{y}.png"
+                    attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.opentopomap.org/">opentopomap.org</a>'
+                />
+              </LayersControl.BaseLayer>
+              <LayersControl.BaseLayer name="Mapbox Outdoors">
+                <TileLayer
+                    id='mapbox/outdoors-v11'
+                    url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGphc3oiLCJhIjoiY2wxcDQ4eG1pMHZxNDNjcGM3djJ4eGphMCJ9.aH-D5oeZHZVzcWQZeeRviQ"
+                    attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+                />
+              </LayersControl.BaseLayer>
+              <LayersControl.BaseLayer name="Mapbox Satellite">
+                <TileLayer
+                    id='mapbox/satellite-v9'
+                    url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGphc3oiLCJhIjoiY2wxcDQ4eG1pMHZxNDNjcGM3djJ4eGphMCJ9.aH-D5oeZHZVzcWQZeeRviQ"
+                    attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+                />
+              </LayersControl.BaseLayer>
+            </LayersControl>
           </MapContainer>
         </div>
       );
