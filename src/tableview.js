029604bfb1d72bdbefea2d8ac0952fbe27ed42f5
tjasz <nukorausa@hotmail.com>
2022-10-23 18:01:24 -0700

    Add table style

--- src/tableview.js
+++ src/tableview.js
@@ -14,12 +14,22 @@ class TableView extends React.Component {
     render() {
       return (
         <div id="tableview" style={this.props.style}>
-          <table cellSpacing={0}>
+          <table id="data-table" cellSpacing={0}>
             <tbody>
               <tr>
-                <td>Name</td>
-                <td>Lat</td>
-                <td>Lon</td>
+                <th>Name</th>
+                <th>Lat</th>
+                <th>Lon</th>
+              </tr>
+              <tr>
+                <td>Seattle</td>
+                <td>47.5</td>
+                <td>-122.3</td>
+              </tr>
+              <tr>
+                <td>Null Island</td>
+                <td>0</td>
+                <td>0</td>
               </tr>
             </tbody>
           </table>
