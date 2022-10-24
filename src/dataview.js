ad79048fe36c654bdb814b7e50a67aa4d644a85e
tjasz <nukorausa@hotmail.com>
2022-10-24 09:59:43 -0700

    Allow uploading of JSON files

--- src/dataview.js
+++ src/dataview.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {DataContext} from './dataContext.js'
 
 function DataView(props) {
-      return (
-        <div id="dataview" style={props.style}>
-          <input type="file" id="file-selector" />
-          <input type="button" id="next-button" value="Process" />
-          <p id="getting-started">Need an example file to try to the viewer? Try <a href="data/Backpacking_Washington.json">Backpacking Washington</a>.</p>
-        </div>
-      );
-    }
+  const context = useContext(DataContext);
+  const process = () => {
+    const fileSelector = document.getElementById('file-selector');
+    const fname = fileSelector.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+      const jso = JSON.parse(event.target.result);
+      context.setData(jso);
+    });
+    reader.readAsText(fname);
+  };
+  return (
+    <div id="dataview" style={props.style}>
+      <input type="file" id="file-selector" />
+      <input type="button" id="next-button" value="Process" onClick={process} />
+      <p id="getting-started">Need an example file to try to the viewer? Try <a href="data/Backpacking_Washington.json">Backpacking Washington</a>.</p>
+    </div>
+  );
+}
 
 export default DataView;
