8590603b15df330cddd9c00a7bcd9bc5eaf4c849
tjasz <nukorausa@hotmail.com>
2022-12-07 10:45:13 -0800

    Smarter lat/lon field detection for CSV

--- src/algorithm.js
+++ src/algorithm.js
@@ -246,10 +246,50 @@ export function getFeatureListBounds(features) {
   return bounds.flat().some((bound) => bound === null) ? null : bounds;
 }
 
+function getLatField(fields) {
+  let latfield = fields.find((f) => f.toLowerCase() === "latitude");
+  if (latfield) return latfield;
+
+  latfield = fields.find((f) => f.toLowerCase().startsWith("latitude"));
+  if (latfield) return latfield;
+
+  latfield = fields.find((f) => f.toLowerCase().includes("latitude"));
+  if (latfield) return latfield;
+
+  latfield = fields.find((f) => f.toLowerCase() === "lat");
+  if (latfield) return latfield;
+
+  latfield = fields.find((f) => f.toLowerCase().startsWith("lat"));
+  if (latfield) return latfield;
+
+  latfield = fields.find((f) => f.toLowerCase().includes("lat"));
+  return latfield;
+}
+
+function getLonField(fields) {
+  let lonfield = fields.find((f) => f.toLowerCase() === "longitude");
+  if (lonfield) return lonfield;
+
+  lonfield = fields.find((f) => f.toLowerCase().startsWith("longitude"));
+  if (lonfield) return lonfield;
+
+  lonfield = fields.find((f) => f.toLowerCase().includes("longitude"));
+  if (lonfield) return lonfield;
+
+  lonfield = fields.find((f) => f.toLowerCase() === "lon");
+  if (lonfield) return lonfield;
+
+  lonfield = fields.find((f) => f.toLowerCase().startsWith("lon"));
+  if (lonfield) return lonfield;
+
+  lonfield = fields.find((f) => f.toLowerCase().includes("lon"));
+  return lonfield;
+}
+
 export function csvToJson(csvString) {
   const parseResult = Papa.parse(csvString, {delimiter: ",", header: true, dynamicTyping: false, skipEmptyLines: true});
-  const latfield = parseResult.meta.fields.find((f) => f.toLowerCase().includes("lat"));
-  const lonfield = parseResult.meta.fields.find((f) => f.toLowerCase().includes("lon"));
+  const latfield = getLatField(parseResult.meta.fields);
+  const lonfield = getLonField(parseResult.meta.fields);
   // TODO allow manual specification of lat/lon fields
   // TODO handle non-number lat/lon formats like DMS
   if (!latfield) throw Error(`Expected latitude field. Found ${parseResult.meta.fields}.`);
