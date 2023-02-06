9605fd41eda12235f639f512a6899bfb533eb67f
tjasz <nukorausa@hotmail.com>
2023-02-06 13:42:43 -0800

    Handle more value types in date columns

--- src/tableview.js
+++ src/tableview.js
@@ -527,20 +527,42 @@ function TableCell(props) {
 }
 
 function CellValue(props) {
-  return (
-    props.value !== null && props.value !== undefined &&
-    (props.value === "" ? undefined :
-      typeof props.value === "string" && props.value.startsWith("http")
-      ? <AbridgedUrlLink target="_blank" href={props.value} length={21} />
-      : props.column.type === "number"
-        ? Number(props.value)
-        : props.column.type === "date"
-          ? new Date(typeof props.value === "number" ? props.value : Date.parse(props.value)).toISOString()
-          : typeof props.value === "string" || typeof props.value === "number"
-            ? props.value
-            : JSON.stringify(props.value)
-    )
-  );
+  // transform null, undefined, empty string to undefined
+  if (props.value === null || props.value === undefined || props.value === "") {
+    return undefined;
+  }
+  // transform URLs to links
+  if (typeof props.value === "string" && props.value.startsWith("http")) {
+    return <AbridgedUrlLink target="_blank" href={props.value} length={21} />;
+  }
+  switch (props.column.type) {
+    case "number":
+      try {
+        return Number(props.value);
+      }
+      catch (e) {
+        alert(`Invalid number -- could not parse "${props.value}" to number:\n ${e.message}`);
+      }
+    case "date":
+      try {
+        let date = null;
+        if (typeof props.value === "number") {
+          date = new Date(props.value);
+        } else if (typeof props.value === "string") {
+          // if it contains only digits, convert to a number; otherweise, parse as date string
+          const isnum = props.value.match(/^[0-9]+$/) != null;
+          date = new Date(isnum ? Number(props.value) : Date.parse(props.value));
+        }
+        return date.toISOString();
+      }
+      catch (e) {
+        alert(`Invalid date -- could not parse "${props.value}" to date:\n ${e.message}`);
+      }
+    default:
+      return typeof props.value === "string" || typeof props.value === "number"
+      ? props.value
+      : JSON.stringify(props.value);
+  }
 }
 
 export default TableView;
