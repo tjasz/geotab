fd3d4cd1e740e803dc7756f8f62fe4de08a3b31f
tj <nukorausa@hotmail.com>
2023-03-02 21:30:09 -0800

    Format the file info dialog based on google drive properties definition (#15)

--- src/GoogleDriverPickerDialog.tsx
+++ src/GoogleDriverPickerDialog.tsx
@@ -1,71 +1,10 @@
-import CSS from 'csstype'
 import {useRef, useState} from 'react'
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-
-type DriveFile = {
-  id:string,
-  mimeType:string,
-  name:string,
-  modifiedByMeTime:string,
-  size:number
-};
-type PropertyDefinition = {
-  id:string,
-  name:string,
-  visible:boolean,
-  align:CSS.Property.TextAlign,
-  display:(v:any) => string
-}
-const FileSizePrefixes = ["B", "KiB", "MiB", "GiB", "TiB", "PiB"];
-const driveFileProperties:PropertyDefinition[] = [
-  {
-    id:"id",
-    name:"ID",
-    visible:false,
-    align:"left",
-    display:(v:string) => v
-  },
-  {
-    id:"mimeType",
-    name:"MIME Type",
-    visible:false,
-    align:"left",
-    display:(v:string) => v
-  },
-  {
-    id:"name",
-    name:"Name",
-    visible:true,
-    align:"left",
-    display:(v:string) => v
-  },
-  {
-    id:"modifiedByMeTime",
-    name:"Modified By Me",
-    visible:true,
-    align:"left",
-    display:(v:string) => v
-  },
-  {
-    id:"size",
-    name:"Size",
-    visible:true,
-    align:"right",
-    display:(v:string) => {
-      let n = Number(v);
-      let powerOf1024 = 0;
-      while (n > 1024) {
-        n = n / 1024;
-        powerOf1024++;
-      }
-      return Math.round(n).toString() + " " + FileSizePrefixes[powerOf1024];
-    }
-  },
-]
+import {DriveFile, driveFileProperties} from './GoogleDriveFile'
 
 export function GoogleDrivePickerDialog(props) {
   const ref = useRef<HTMLElement>(null);
