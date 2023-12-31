68b5952ccfa56d188e4b26c3b8788c1bca49976c
tj <nukorausa@hotmail.com>
2023-12-30 19:40:06 -0800

    Add buffer options to export (#82)

--- src/dataview.js
+++ src/dataview.js
@@ -1,5 +1,7 @@
 import React, {useContext, useState} from 'react';
 import { useSearchParams } from "react-router-dom";
+import CircularProgress from '@mui/material/CircularProgress';
+import {buffer, combine, union} from "@turf/turf"
 import {DataContext} from './dataContext'
 import {defaultFilter, ConditionOperator, ConditionGroupOperator, parametersMap, operandTypes, Condition, ConditionGroup, filterEquals, FilterType, validateFilter} from './filter'
 import {FieldTypeDescription} from './fieldtype'
@@ -151,34 +153,102 @@ function FileImporter({onRead}) {
 
 function ExportView(props) {
   const context = useContext(DataContext);
+  const [exportOption, setExportOption] = useState("all");
+  const [isLoading, setIsLoading] = useState(false);
   const isActive = (feature) => {
     const status = feature.properties["geotab:selectionStatus"];
     return status !== undefined && !status.includes("inactive");
   };
-  const exportJson = (includeHidden, filterFunc = f => true) => {
-    const downloadLink = document.createElement("a");
-    const features = (includeHidden ? context.data : context.filteredData).filter(filterFunc);
-    const textContent = JSON.stringify({
+
+  const createBuffer = (features) => {
+    const bufferDistances = [0.1, 0.2, 0.4, 0.8, 1.6];
+    const bufferFeatures = bufferDistances.map(dist =>
+      features.slice(1).reduce(
+        (cumulativeBuffer, feature) => union(cumulativeBuffer, buffer(feature, dist, {units: "kilometers"}), {properties: { bufferDistance: dist}}),
+        buffer(features[0], dist, {units: "kilometers"})
+        ));
+    return {
       type: "FeatureCollection",
-      features,
-      geotabMetadata: {
-        columns: context.columns,
-        filter: context.filter,
-        symbology: context.symbology
-      }
-    });
+      features: bufferFeatures,
+    };
+  }
+
+  const createExportFeature = (includeHidden, filterFunc = f => true, buffer = false) => {
+      const features = (includeHidden ? context.data : context.filteredData).filter(filterFunc);
+      return buffer
+        ? createBuffer(features)
+        : {
+          type: "FeatureCollection",
+          features,
+          geotabMetadata: {
+            columns: context.columns,
+            filter: context.filter,
+            symbology: context.symbology
+          }
+        };
+  }
+
+  const exportToFile = (featureCollection) => {
+    const textContent = JSON.stringify(featureCollection);
     const file = new Blob([textContent], {type: 'text/plain'});
+    const downloadLink = document.createElement("a");
     downloadLink.href = URL.createObjectURL(file);
     downloadLink.download = "geotabExport.json";
     document.body.appendChild(downloadLink); // Required for this to work in FireFox
     downloadLink.click();
   };
+
+  const doExport = () => {
+    // set parameters from export option
+    var includeHidden = true;
+    var filter = f => true;
+    var buffer = false;
+    switch(exportOption) {
+      case "all":
+        break;
+      case "filtered":
+        includeHidden = false;
+        break;
+      case "selected":
+        includeHidden = false;
+        filter = isActive;
+        break;
+      case "bufferAll":
+        buffer = true;
+        break;
+      case "bufferFiltered":
+        includeHidden = false;
+        buffer = true;
+        break;
+      case "bufferSelected":
+        includeHidden = false;
+        filter = isActive;
+        buffer = true;
+        break;
+    }
+
+    const featureCollection = createExportFeature(includeHidden, filter, buffer);
+    exportToFile(featureCollection);
+    setIsLoading(false);
+  };
+
   return (
     <div id="exportView">
       <h3>Export</h3>
-      <button onClick={() => exportJson(true)}>Export All</button>
-      <button onClick={() => exportJson(false)}>Export Filtered</button>
-      <button onClick={() => exportJson(false, isActive)}>Export Selected</button>
+      <select defaultValue={exportOption} onChange={(ev) => setExportOption(ev.target.value)}>
+        <option value="all">All</option>
+        <option value="filtered">Filtered</option>
+        <option value="selected">Selected</option>
+        <option value="bufferAll">Buffer (All)</option>
+        <option value="bufferFiltered">Buffer (Filtered)</option>
+        <option value="bufferSelected">Buffer (Selected)</option>
+      </select>
+      {isLoading ? <CircularProgress /> : <button onClick={() => {
+        setIsLoading(true);
+        setTimeout(doExport, 0);
+        }}>
+      Export
+      </button>}
     </div>
   );
 }
