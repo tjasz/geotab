e9375c009f373b89d226dbf5696eaf2b54aefbe0
tjasz <nukorausa@hotmail.com>
2024-06-02 20:15:24 -0700

    Beef up the CSV parser optiosn

--- src/file-formats/csvToGeojson.ts
+++ src/file-formats/csvToGeojson.ts
@@ -1,79 +1,97 @@
 import Papa from "papaparse";
-import * as GeoJson from "../geojson-types";
+import { featureCollection, FeatureCollection, point } from "@turf/turf";
 
-export function csvToGeoJSON(csvString: string): GeoJson.FeatureCollection {
+enum InvalidPositionHandling {
+  Throw,
+  SkipRow,
+  ReplaceWithNull,
+};
+
+type ICsvToGeoJsonOptions = {
+  delimiter?: string;
+  latitudeField?: string;
+  longitudeField?: string;
+  parseLatLon?: (string) => number;
+  invalidPositionHandling?: InvalidPositionHandling;
+};
+
+export function csvToGeoJSON(csvString: string, options?: ICsvToGeoJsonOptions): FeatureCollection {
   const parseResult = Papa.parse(csvString, {
-    delimiter: ",",
+    delimiter: options?.delimiter ?? ",",
     header: true,
     dynamicTyping: false,
     skipEmptyLines: true,
   });
-  const latfield = getLatField(parseResult.meta.fields);
-  const lonfield = getLonField(parseResult.meta.fields);
-  // TODO allow manual specification of lat/lon fields
-  // TODO handle non-number lat/lon formats like DMS
-  if (!latfield)
-    throw Error(`Expected latitude field. Found ${parseResult.meta.fields}.`);
-  if (!lonfield)
-    throw Error(`Expected longitude field. Found ${parseResult.meta.fields}.`);
 
-  return {
-    type: GeoJson.FeatureType.FeatureCollection,
-    features: parseResult.data.map((row) => {
-      const lat = parseFloat(row[latfield]);
-      const lon = parseFloat(row[lonfield]);
-      const geometry =
-        isNaN(lat) || isNaN(lon)
-          ? null
-          : {
-            type: "Point",
-            coordinates: [lon, lat],
-          };
+  // Get latitude and longitude fields
+  const latfield = options?.latitudeField ?? findField(parseResult.meta.fields, ["latitude", "lat"]);
+  const lonfield = options?.latitudeField ?? findField(parseResult.meta.fields, ["longitude", "lon", "lng"]);
+  if (!latfield) {
+    throw Error(`Expected latitude field. Found ${parseResult.meta.fields}.`);
+  }
+  if (!lonfield) {
+    throw Error(`Expected longitude field. Found ${parseResult.meta.fields}.`);
+  }
+
+  const parseLatLng = options?.parseLatLon ?? parseFloat;
+  const invalidPositionHandling = options?.invalidPositionHandling ?? InvalidPositionHandling.ReplaceWithNull;
+
+  return featureCollection(parseResult.data.map((row) => {
+    const lat = parseLatLng(row[latfield]);
+    const lon = parseLatLng(row[lonfield]);
+
+    const latValidation = validateNumber(lat, 90, -90);
+    const lonValidation = validateNumber(lon, 180, -180);
+
+    if (invalidPositionHandling === InvalidPositionHandling.Throw) {
+      if (!latValidation[0]) {
+        throw Error(`Invalid latitude - ${latValidation[1]} : ${row[latfield]}`)
+      }
+      if (!lonValidation[0]) {
+        throw Error(`Invalid longitude - ${lonValidation[1]} : ${row[lonfield]}`)
+      }
+    }
+
+    if (
+      invalidPositionHandling === InvalidPositionHandling.ReplaceWithNull ||
+      invalidPositionHandling === InvalidPositionHandling.SkipRow
+    ) {
       return {
-        type: "Feature",
-        geometry,
+        type: "Point",
+        geometry: null,
         properties: row,
       };
-    }),
-  };
+    }
+
+    return point([lon, lat], row);
+  })
+    .filter(f => !(invalidPositionHandling === InvalidPositionHandling.SkipRow && f.geometry === null))
+  );
 }
 
-function getLatField(fields: string[]): string | undefined {
-  let latfield = fields.find((f) => f.toLowerCase() === "latitude");
-  if (latfield) return latfield;
-
-  latfield = fields.find((f) => f.toLowerCase().startsWith("latitude"));
-  if (latfield) return latfield;
-
-  latfield = fields.find((f) => f.toLowerCase().includes("latitude"));
-  if (latfield) return latfield;
-
-  latfield = fields.find((f) => f.toLowerCase() === "lat");
-  if (latfield) return latfield;
-
-  latfield = fields.find((f) => f.toLowerCase().startsWith("lat"));
-  if (latfield) return latfield;
-
-  latfield = fields.find((f) => f.toLowerCase().includes("lat"));
-  return latfield;
+// Find a field that either matches, starts with, or includes one of the preferred names,
+// in order of preference.
+function findField(fields: string[], preferredNames: string[]): string | undefined {
+  for (const name of preferredNames) {
+    const field = fields.find((f) => f.toLowerCase() === name) ??
+      fields.find((f) => f.toLowerCase().startsWith(name)) ??
+      fields.find((f) => f.toLowerCase().includes(name));
+    if (field) {
+      return field;
+    }
+  }
+  return undefined;
 }
 
-function getLonField(fields: string[]): string | undefined {
-  let lonfield = fields.find((f) => f.toLowerCase() === "longitude");
-  if (lonfield) return lonfield;
-
-  lonfield = fields.find((f) => f.toLowerCase().startsWith("longitude"));
-  if (lonfield) return lonfield;
-
-  lonfield = fields.find((f) => f.toLowerCase().includes("longitude"));
-  if (lonfield) return lonfield;
-
-  lonfield = fields.find((f) => f.toLowerCase() === "lon");
-  if (lonfield) return lonfield;
-
-  lonfield = fields.find((f) => f.toLowerCase().startsWith("lon"));
-  if (lonfield) return lonfield;
-
-  lonfield = fields.find((f) => f.toLowerCase().includes("lon"));
-  return lonfield;
+function validateNumber(val: number, max: number, min: number): [boolean, string?] {
+  if (isNaN(val)) {
+    return [false, "not a number"]
+  }
+  if (val > max) {
+    return [false, `cannot exceed ${max}`];
+  }
+  if (val < min) {
+    return [false, `cannot be less than ${min}`];
+  }
+  return [true];
 }
