48d6faa7b4b8b8115d5d99b03daeb69b2a3b2c3b
tjasz <nukorausa@hotmail.com>
2024-06-11 13:48:31 -0700

    Add min and max width

--- src/SplitView.tsx
+++ src/SplitView.tsx
@@ -6,20 +6,24 @@ type SplitViewProps = {
 };
 
 export function SplitView(props: SplitViewProps) {
+  const minWidth = "50px";
   const count = Children.count(props.children);
   const defaultPaneWidth = 300; //`calc((100% - (${count - 1} * ${dividerWidth})) / ${count})`; TODO
+  const maxWidth = `calc(100% - (${count - 1} * ${minWidth}))`
   const [widths, setWidths] = useState<number[]>(Array(count - 1).fill(defaultPaneWidth));
 
   const children = Children.toArray(props.children);
 
   console.log(widths)
 
-  return <div className="splitview" style={{ width: "100%" }}>
+  return <div className="splitview" style={{ width: "100%", overflow: "hidden" }}>
     {children.slice(0, -1).map((child, idx) =>
       <Pane
         resize="horizontal"
         initialWidth={`${widths[idx]}px`}
-        onWidthChange={newWidth => setWidths(widths.map((v, i) => idx === i ? newWidth : v))}
+        onWidthChange={newWidth => setWidths(widths.map((v, i) => idx === i ? Math.floor(newWidth) : v))}
+        minWidth={minWidth}
+        maxWidth={maxWidth}
       >
         {child}
       </Pane>
@@ -28,6 +32,8 @@ export function SplitView(props: SplitViewProps) {
       resize="none"
       initialWidth={`calc(100% - ${widths.reduce((sum, v) => sum + v, 0)}px)`}
       onWidthChange={() => { }}
+      minWidth={minWidth}
+      maxWidth={maxWidth}
     >
       {children.at(children.length - 1)}
     </Pane>
@@ -38,18 +44,26 @@ type PaneProps = {
   children: ReactNode,
   resize: "horizontal" | "none",
   initialWidth: string,
+  maxWidth: string,
+  minWidth: string,
   onWidthChange: (v: number) => void,
 }
 
 function Pane(props: PaneProps) {
   const [ref, { width, height }] = useMeasure();
 
-
-
   return <div
     ref={ref}
     className="splitview-pane"
-    style={{ resize: props.resize, overflow: "scroll", width: props.initialWidth, float: "left" }}
+    style={{
+      overflow: "scroll",
+      float: "left",
+      resize: props.resize,
+      width: props.initialWidth,
+      maxWidth: props.maxWidth,
+      minWidth: props.minWidth,
+      padding: 0,
+    }}
     onMouseMove={() => props.onWidthChange(width ?? 0)}
   >
     {props.children}
