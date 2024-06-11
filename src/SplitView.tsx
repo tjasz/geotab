d7b5d7f2c815306a1f7f30f0ec13f152a8ef4517
tjasz <nukorausa@hotmail.com>
2024-06-11 12:35:43 -0700

    Add state to pane widths

--- src/SplitView.tsx
+++ src/SplitView.tsx
@@ -1,41 +1,41 @@
 import React, { ReactNode, Children } from "react";
-import DataView from "./dataview";
-import MapView from "./mapview";
-import TableView from "./table/tableview";
-import SymbologyView from "./symbologyview";
 
 type SplitViewProps = {
   children: ReactNode
 };
-type SplitViewState = {};
+type SplitViewState = {
+  widths: string[];
+};
 
 const dividerWidth = "10px";
 
 export class SplitView extends React.Component<SplitViewProps, SplitViewState> {
   constructor(props: SplitViewProps) {
     super(props);
+
+    const count = Children.count(this.props.children);
+    const defaultPaneWidth = `calc((100% - (${count - 1} * ${dividerWidth})) / ${count})`;
+    this.state = {
+      widths: Array(count).fill(defaultPaneWidth)
+    }
   }
 
   render() {
-    const count = Children.count(this.props.children);
-
-    const paneWidth = `calc((100% - (${count - 1} * ${dividerWidth})) / ${count})`;
-
     const children = Children.toArray(this.props.children);
 
-    const toPane = (child) =>
-      <div className="splitview-pane" style={{ width: paneWidth, float: "left" }}>
+    const toPane = (child, i: number) =>
+      <div className="splitview-pane" style={{ width: this.state.widths[i], float: "left" }}>
         {child}
       </div>
 
     return <div className="splitview" style={{ width: "100%" }}>
-      {children.slice(0, -1).map(child =>
+      {children.slice(0, -1).map((child, i) =>
         <>
-          {toPane(child)}
+          {toPane(child, i)}
           <div className="splitview-divider" style={{ height: "100px", width: dividerWidth, float: "left", backgroundColor: "black" }}></div>
         </>
       )}
-      {toPane(children.at(children.length - 1))}
+      {toPane(children.at(children.length - 1), children.length - 1)}
     </div>
   }
 }
