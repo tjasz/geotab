1734ca554ad86655e32b9b93ef1ffd3e66dcb73c
tjasz <nukorausa@hotmail.com>
2024-06-13 11:27:27 -0700

    Add tick marks perpendicular to path

--- src/SvgPatternRenderer.ts
+++ src/SvgPatternRenderer.ts
@@ -16,8 +16,17 @@ function pointsToPatternPath(rings, closed) {
     for (j = 0, len2 = points.length; j < len2; j++) {
       p = points[j];
       str += `${(j ? 'L' : 'M') + p.x} ${p.y}`;
-      // add vertical tick mark
-      str += "m0 -5 v10 m0 -5"
+      // add tick marks perpendicular to path
+      if (j) {
+        const tickSize = 10;
+        const segmentBearing = Math.atan2(p.y - points[j - 1].y, p.x - points[j - 1].x);
+        const tickBearing = segmentBearing + Math.PI / 2;
+        const tickDx = tickSize * Math.cos(tickBearing);
+        const tickDy = tickSize * Math.sin(tickBearing);
+        str += `m${-tickDx} ${-tickDy} l${2 * tickDx} ${2 * tickDy}`;
+        // return to original point
+        str += `M${p.x} ${p.y}`;
+      }
     }
 
     // closes the ring for polygons
