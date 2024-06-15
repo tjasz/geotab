2e9ad64a45bd9d82e163199d074ef2405216c0bf
tjasz <nukorausa@hotmail.com>
2024-06-15 10:01:23 -0700

    Simplify the pattern renderer loop

--- src/PatternRenderer/SvgPatternRenderer.ts
+++ src/PatternRenderer/SvgPatternRenderer.ts
@@ -28,69 +28,64 @@ export function pointsToPatternPath(rings: Point[][], closed: boolean, patternSt
   let str = '',
     i, j, len, len2, points, p: { x: number, y: number };
 
-  if (pattern === "solid") {
+  // if a polygon, pattern is "solid" or any pattern part has type "T", draw the whole polyline first
+  // Note that this means polygons essentially don't support "F" type patterns
+  if (closed || pattern === "solid" || pattern.some(part => part.type === "T")) {
     for (i = 0, len = rings.length; i < len; i++) {
       points = rings[i];
       for (j = 0, len2 = points.length; j < len2; j++) {
         p = points[j];
-        if (j) {
-          str += `L${p.x} ${p.y}`;
-        }
-        else {
+        // If this is the first point of a ring, move the marker to it and draw nothing
+        if (!j) {
           str += `M${p.x} ${p.y}`;
         }
+        else {
+          str += `L${p.x} ${p.y}`;
+        }
       }
+      // closes the ring for polygons
+      str += closed ? 'z' : '';
     }
   }
-  else {
+
+  // then draw the patterns
+  if (pattern !== "solid") {
     for (i = 0, len = rings.length; i < len; i++) {
       points = rings[i];
       let leftoverDistances = pattern.map(p => typeof p.offset === "number" ? p.offset : 0);
 
-      for (j = 0, len2 = points.length; j < len2; j++) {
+      for (j = 1, len2 = points.length; j < len2; j++) {
         p = points[j];
 
-        if (j) {
-          // TODO this assumes that when offset is 100%, the type is T and no other pattern parts are defined
-          if (pattern[0].offset === "100%") {
-            str += `L${p.x} ${p.y}`;
-            // draw pattern at last point
-            if (j === len2 - 1) {
-              const prevPoint = points[j - 1];
-              const segmentBearing = Math.atan2(p.y - prevPoint.y, p.x - prevPoint.x);
-              str += toString(translate(rotate(pattern[0].path, segmentBearing + Math.PI / 2), p.x, p.y))
-            }
-          }
-          else {
-            const prevPoint = points[j - 1];
-            const segmentBearing = Math.atan2(p.y - prevPoint.y, p.x - prevPoint.x);
-            const segmentDist = dist(prevPoint, p);
-
-            for (let patternPart = 0; patternPart < pattern.length; patternPart++) {
-              let k = leftoverDistances[patternPart];
-              for (; k < segmentDist; k += pattern[patternPart].interval ?? 20) {
-                const pk = moveAlongBearing(prevPoint, k, segmentBearing);
-                // move the marker to this point
-                str += `${pattern[patternPart].type === "F" ? "M" : "L"}${pk.x} ${pk.y}`;
-                // draw the pattern
-                // pattern is defined with positive y as the direction of travel,
-                // but these bearings assume positive x is direction of travel, so rotate 90 extra degrees
-                str += toString(translate(rotate(pattern[patternPart].path, segmentBearing + Math.PI / 2), pk.x, pk.y))
-                // return to original point
-                str += `M${pk.x} ${pk.y}`;
-              }
-              // set leftover distance and move to end of segment
-              str += `${pattern[patternPart].type === "F" ? "M" : "L"}${p.x} ${p.y}`;
-              leftoverDistances[patternPart] = k - segmentDist;
-            }
-          }
-        } else {
+        const prevPoint = points[j - 1];
+        const segmentBearing = Math.atan2(p.y - prevPoint.y, p.x - prevPoint.x);
+        // TODO this assumes that when offset is 100%, no other pattern parts are defined
+        if (pattern[0].offset === "100%") {
+          // draw pattern at last point
           str += `M${p.x} ${p.y}`;
+          if (j === len2 - 1) {
+            str += toString(translate(rotate(pattern[0].path, segmentBearing + Math.PI / 2), p.x, p.y))
+          }
+        }
+        else {
+          const segmentDist = dist(prevPoint, p);
+
+          for (let patternPart = 0; patternPart < pattern.length; patternPart++) {
+            let k = leftoverDistances[patternPart];
+            for (; k < segmentDist; k += pattern[patternPart].interval) {
+              const pk = moveAlongBearing(prevPoint, k, segmentBearing);
+              // move the marker to this point
+              str += `M${pk.x} ${pk.y}`;
+              // draw the pattern
+              // pattern is defined with positive y as the direction of travel,
+              // but these bearings assume positive x is direction of travel, so rotate 90 extra degrees
+              str += toString(translate(rotate(pattern[patternPart].path, segmentBearing + Math.PI / 2), pk.x, pk.y))
+            }
+            // set leftover distance
+            leftoverDistances[patternPart] = k - segmentDist;
+          }
         }
       }
-
-      // closes the ring for polygons
-      str += closed ? 'z' : '';
     }
   }
 
