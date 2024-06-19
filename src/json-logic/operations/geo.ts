71c0da3b65d553d486ac9d35e2893f909cf178f0
tj <nukorausa@hotmail.com>
2024-06-18 17:35:53 -0700

    Geo function for json-logic: find steepest interval on a path (#107)

--- src/json-logic/operations/geo.ts
+++ src/json-logic/operations/geo.ts
@@ -1,6 +1,7 @@
 import * as Turf from "@turf/turf";
 import { getEndingCoord, getStartingCoord } from "../../algorithm";
 import { pathNovelty } from "../../geojson-calc";
+import { FeatureType, GeometryType } from "../../geojson-types";
 
 type OperatorBody = (...args: any[]) => any;
 
@@ -82,6 +83,81 @@ const toOutAndBack: OperatorBody = (feature, point) => {
   }
 };
 
+const steepestInterval: OperatorBody = (feature, intervalMeters: number) => {
+  if (intervalMeters === undefined) {
+    throw new Error("Cannot compute steepest interval of undefined length")
+  }
+  intervalMeters = Number(intervalMeters)
+  if (!intervalMeters) {
+    throw new Error("Cannot compute steepest interval of 0 meters");
+  }
+  switch (feature.geometry.type) {
+    case "Point":
+    case "MultiPoint":
+      throw new Error("steepestInterval not implemented for this geometry type");
+    case "LineString":
+      const coords = feature.geometry.coordinates;
+      if (coords[0].length < 3) {
+        throw new Error("Cannot calculate steepest interval on feature with only 2d coordinates");
+      }
+      // pre compute distance and gain array so that it doesn't need repeating during looping
+      const distances = coords.map((c, i) => i ? Turf.distance(c, coords[i - 1], { units: "meters" }) : 0);
+      const gains = coords.map((c, i) => i ? Math.max(0, c[2] - coords[i - 1][2]) : 0);
+
+      let i = 0, j = 0, currentIntervalDist = 0, currentIntervalGain = 0;
+      // increment j until it defines an interval of the desired length
+      while (currentIntervalDist < intervalMeters && j < coords.length) {
+        // add the distance and positive elevation change to the interval stats
+        j++;
+        currentIntervalDist += distances[j];
+        currentIntervalGain += gains[j];
+      }
+      // if path is shorter than the interval, there is no steepest interval
+      if (j === coords.length - 1) {
+        return undefined;
+      }
+      // iterate over intervals
+      let winner = { i, j, length: currentIntervalDist, gain: currentIntervalGain, grade: currentIntervalGain / currentIntervalDist };
+      while (i < coords.length && j < coords.length) {
+        // if interval is too long, advance i
+        while (currentIntervalDist > intervalMeters && i < coords.length) {
+          currentIntervalDist -= distances[i];
+          currentIntervalGain -= gains[i];
+          i += 1;
+        }
+        // then, consider if current gain is the most
+        const currentGrade = currentIntervalGain / currentIntervalDist;
+        if (currentGrade > winner.grade) {
+          winner = { i, j, length: currentIntervalDist, gain: currentIntervalGain, grade: currentGrade };
+        }
+        // advance j
+        do {
+          j += 1;
+          currentIntervalDist += distances[j];
+          currentIntervalGain += gains[j];
+        } while (currentIntervalDist < intervalMeters && j < coords.length)
+      }
+      const result = {
+        type: FeatureType.Feature,
+        geometry: {
+          type: GeometryType.LineString,
+          coordinates: coords.slice(winner.i, winner.j + 1),
+        },
+        properties: {
+          lengthMeters: winner.length,
+          gainMeters: winner.gain,
+          grade: winner.grade,
+        }
+      };
+      return result;
+    case "MultiLineString":
+    case "Polygon":
+    case "MultiPolygon":
+    case "GeometryCollection":
+      throw new Error("steepestInterval not implemented for this geometry type");
+  }
+}
+
 export const Geo = {
   beginning,
   ending,
@@ -89,6 +165,7 @@ export const Geo = {
   unionMany,
   distanceToPoint,
   toOutAndBack,
+  steepestInterval,
 };
 
 export default Geo;
