0c2a79a2c4196124f9cb6c8ccec18b9becbd7474
tjasz <nukorausa@hotmail.com>
2024-12-13 20:06:38 -0800

    Add more mapbox layers

--- src/maplayers.ts
+++ src/maplayers.ts
@@ -83,6 +83,45 @@ const baseLayers = [
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   },
+  {
+    type: "TileLayer",
+    name: "Mapbox Satellite",
+    geotabId: "mbsat",
+    checked: false,
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+    id: "mapbox/satellite-v9",
+    url: "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGphc3oiLCJhIjoiY2wxcDQ4eG1pMHZxNDNjcGM3djJ4eGphMCJ9.aH-D5oeZHZVzcWQZeeRviQ",
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  },
+  {
+    type: "TileLayer",
+    name: "Mapbox Light",
+    geotabId: "mblight",
+    checked: false,
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+    id: "mapbox/light-v11",
+    url: "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGphc3oiLCJhIjoiY2wxcDQ4eG1pMHZxNDNjcGM3djJ4eGphMCJ9.aH-D5oeZHZVzcWQZeeRviQ",
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  },
+  {
+    type: "TileLayer",
+    name: "Mapbox Dark",
+    geotabId: "mbdark",
+    checked: false,
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+    id: "mapbox/dark-v11",
+    url: "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGphc3oiLCJhIjoiY2wxcDQ4eG1pMHZxNDNjcGM3djJ4eGphMCJ9.aH-D5oeZHZVzcWQZeeRviQ",
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  },
   makeTnmBaseMap('basemap', 'USGSTopo'),
   {
     type: "WMSTileLayer",
