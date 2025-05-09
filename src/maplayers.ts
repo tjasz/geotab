de20fd5cf657b167acfcb1eeee9e939470f0a440
tjasz <nukorausa@hotmail.com>
2025-05-09 14:00:19 -0700

    Remove duplicate Mapbox Satellite layer

--- src/maplayers.ts
+++ src/maplayers.ts
@@ -85,19 +85,6 @@ const baseLayers = [
   },
   {
     type: "TileLayer",
-    name: "Mapbox Satellite",
-    geotabId: "mbsat",
-    checked: false,
-    maxZoom: 18,
-    tileSize: 512,
-    zoomOffset: -1,
-    id: "mapbox/satellite-v9",
-    url: "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGphc3oiLCJhIjoiY2wxcDQ4eG1pMHZxNDNjcGM3djJ4eGphMCJ9.aH-D5oeZHZVzcWQZeeRviQ",
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  },
-  {
-    type: "TileLayer",
     name: "Mapbox Light",
     geotabId: "mblight",
     checked: false,
